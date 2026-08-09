e62bc52d6993a838ff3caae1b09ed3ba5f60a659
ljj <1412415464@qq.com>
2026-08-09 09:37:56 +0800

    创建基础架构

---
+++ src/main/java/com/example/CWAIAgent/CwAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.CWAIAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/CWAIAgent/CwAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CWAIAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
